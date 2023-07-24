b1e8e84cd5096b3572c8a1e6a5dcb837987a4cf9
jiftechnify <jiftech.stlfy@gmail.com>
2023-07-24 23:40:40 +0900

    add options: since, until, skip-verification

--- deps.ts
+++ deps.ts
@@ -1,1 +1,3 @@
+export { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts";
 export { NostrFetcher } from "npm:nostr-fetch@0.12.1";
+export type { AllEventsIterOptions } from "npm:nostr-fetch@0.12.1";
--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
-import { NostrFetcher } from "./deps.ts";
+import { Command, NostrFetcher } from "./deps.ts";
+
+const { args, options } = await new Command()
+  .name("nosdump")
+  .version("0.2.0")
+  .description("Dump events stored in Nostr relays")
+  .option("--since <unixtime:number>", "fetch events newer than the timestamp")
+  .option("--until <unixtime:number>", "fetch events older than the timestamp")
+  .option("--skip-verification", "skip event signature verification")
+  .arguments("<relay-URLs...>")
+  .parse(Deno.args);
+
+console.log("options: %O", options);
 
 const fetcher = NostrFetcher.init();
-for await (const ev of fetcher.allEventsIterator(Deno.args, {}, {})) {
+for await (const ev of fetcher.allEventsIterator(args, {}, options, options)) {
   console.log(JSON.stringify(ev));
 }
 fetcher.shutdown();
