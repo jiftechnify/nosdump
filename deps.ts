584de0716909dd32961558da225cc49ca6f8a268
jiftechnify <jiftech.stlfy@gmail.com>
2023-09-02 02:12:08 +0900

    update deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { readAllSync } from "https://deno.land/std@0.195.0/streams/mod.ts";
+export { readAllSync } from "https://deno.land/std@0.201.0/streams/mod.ts";
 
 export {
   Command,
@@ -13,12 +13,12 @@ export {
   GithubProvider,
 } from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/upgrade/mod.ts";
 
-export { NostrFetcher } from "npm:nostr-fetch@0.12.2";
+export { NostrFetcher } from "npm:nostr-fetch@0.13.0";
 export type {
   AllEventsIterOptions,
   FetchFilter,
   FetchTimeRangeFilter,
-} from "npm:nostr-fetch@0.12.2";
+} from "npm:nostr-fetch@0.13.0";
 
 export { nip19 } from "npm:nostr-tools@1.13.1";
 
