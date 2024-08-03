afda44e2b7a96aef29b5b239f1e40e348a052860
GitHub <noreply@github.com>
2024-08-03 09:19:03 +0000

    chore(deps): Update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,17 +1,17 @@
-export { toText as streamToText } from "https://deno.land/std@0.221.0/streams/mod.ts";
+export { toText as streamToText } from "https://deno.land/std@0.224.0/streams/mod.ts";
 
 export {
   Command,
   CompletionsCommand,
   UpgradeCommand,
   ValidationError,
-} from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
-export type { ArgumentValue } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
+} from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
+export type { ArgumentValue } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
 
 export {
   DenoLandProvider,
   GithubProvider,
-} from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/upgrade/mod.ts";
+} from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/upgrade/mod.ts";
 
 export { NostrFetcher } from "npm:nostr-fetch@0.15.1";
 export type {
@@ -20,11 +20,11 @@ export type {
   FetchTimeRangeFilter,
 } from "npm:nostr-fetch@0.15.1";
 
-export { nip19 } from "npm:nostr-tools@2.4.0";
+export { nip19 } from "npm:nostr-tools@2.7.1";
 
-export { z, ZodError } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+export { z, ZodError } from "https://deno.land/x/zod@v3.23.8/mod.ts";
 
-export { fromZodError } from "npm:zod-validation-error@3.1.0";
+export { fromZodError } from "npm:zod-validation-error@3.3.1";
 
 export {
   getUnixTime,
