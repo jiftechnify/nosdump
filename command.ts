ad0572da5367252480d55f8d122c3415b5dd5b9b
jiftechnify <jiftech.stlfy@gmail.com>
2024-09-25 23:44:13 +0900

    bump version

--- command.ts
+++ command.ts
@@ -25,7 +25,7 @@ import { Result } from "./types.ts";
 
 export const nosdumpCommand = new Command()
   .name("nosdump")
-  .version("0.7.0")
+  .version("0.7.1")
   .description("A tool to dump events stored in Nostr relays")
   .usage("[options...] <relays...>")
   .command("completions", new CompletionsCommand())
